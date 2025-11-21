52cd2a1aa4ab903914dae48c08f3172c739e5458
Orange-Piccolo <donnelly-t19@ulster.ac.uk>
2025-11-21 21:47:44 +0000

    Same changes but more

--- Assets/_Game/Code/_BDATO_Scripts/MainGameScene_Scripts/Enemies/PlayerAwarenessController.cs
+++ Assets/_Game/Code/_BDATO_Scripts/MainGameScene_Scripts/Enemies/PlayerAwarenessController.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerAwarenessController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public bool AwareOfPlayer { get; private set; }
+
+    public Vector2 DirectionToPlayer { get; private set; }
+
+    [SerializeField]
+    private float _playerAwarenessDistance;
+
+    private Transform _player;
+    
+    private void Awake()
     {
-        
+        _player = FindFirstObjectByType<PlayerMovement>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 enemyToPlayerVector = _player.position - transform.position;
+        DirectionToPlayer = enemyToPlayerVector.normalized;
+
+        if (enemyToPlayerVector.magnitude <= _playerAwarenessDistance)
+        {
+            AwareOfPlayer = true;
+        }
+        else
+        {
+            AwareOfPlayer = false;
+        }
     }
 }
