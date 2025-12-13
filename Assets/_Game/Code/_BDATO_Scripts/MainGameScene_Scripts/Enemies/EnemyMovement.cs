ac7d9412e465269e75442bb80877810cee215b57
Orange-Piccolo <donnelly-t19@ulster.ac.uk>
2025-12-13 23:59:25 +0000

    Re-added the enemy movement and rotation

--- Assets/_Game/Code/_BDATO_Scripts/MainGameScene_Scripts/Enemies/EnemyMovement.cs
+++ Assets/_Game/Code/_BDATO_Scripts/MainGameScene_Scripts/Enemies/EnemyMovement.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
@@ -8,54 +7,60 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField]
     private float _speed;
 
+    [SerializeField]
+    private float _rotationSpeed;
+
     private Rigidbody2D _rigidbody;
     private PlayerAwarenessController _playerAwarenessController;
     private Vector2 _targetDirection;
-    private float _changeDirectionCooldown;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _playerAwarenessController = GetComponent<PlayerAwarenessController>();
-        _targetDirection = transform.up;
     }
 
     private void FixedUpdate()
     {
-        UpdateTargetDIrection();
+        UpdateTargetDirection();
+        RotateTowardsTarget();
         SetVelocity();
     }
 
-    private void UpdateTargetDIrection()
-    {
-        HandleRandomDirectionChange();
-        HandlePlayerTargeting();
-    }
-
-    private void HandleRandomDirectionChange()
-    {
-        _changeDirectionCooldown -= Time.deltaTime;
-
-        if (_changeDirectionCooldown <= 0)
-        {
-            float angleChange = Random.Range(-90f, 90f);
-            Quaternion rotation = Quaternion.AngleAxis(angleChange,transform.forward);
-            _targetDirection = rotation * _targetDirection;
-
-            _changeDirectionCooldown = Random.Range(1f, 5f);
-        }
-    }
-
-    private void HandlePlayerTargeting()
+    private void UpdateTargetDirection()
     {
         if (_playerAwarenessController.AwareOfPlayer)
         {
             _targetDirection = _playerAwarenessController.DirectionToPlayer;
         }
+        else
+        {
+            _targetDirection = Vector2.zero;
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        if (_targetDirection == Vector2.zero)
+        {
+            return;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetDirection);
+        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+        _rigidbody.SetRotation(rotation);
     }
 
     private void SetVelocity()
     {
-        _rigidbody.linearVelocity = transform.up * _speed;
+        if (_targetDirection == Vector2.zero)
+        {
+            _rigidbody.linearVelocity = Vector2.zero;
+        }
+        else
+        {
+            _rigidbody.linearVelocity = transform.up * _speed;
+        }
     }
 }
